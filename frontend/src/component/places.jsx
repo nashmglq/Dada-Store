e70a69394cf45883a65337973a4b7809c598e438
Nash Maglaqui <nashmglq89@gmail.com>
2025-05-17 16:22:43 +0800

    Feat: Places

--- frontend/src/component/places.jsx
+++ frontend/src/component/places.jsx
@@ -1,37 +1,108 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
 import { ReactTyped } from "react-typed";
+import { Data } from "../data/data";
 
 export const Places = () => {
+  const [count, setCount] = useState(0);
+  const [imageCount, setImageCount] = useState(0);
+
+
+  const nextButton = (e) => {
+    e.preventDefault();
+    setCount(count + 1);
+    setImageCount(0);
+  };
+  const prevButton = (e) => {
+    e.preventDefault();
+    setCount(count - 1);
+    setImageCount(0);
+  };
+
+  const nextImage = (e) => {
+    e.preventDefault();
+    setImageCount(imageCount + 1);
+  };
+  const prevImage = (e) => {
+    e.preventDefault();
+    setImageCount(imageCount - 1);
+  };
+
+  useEffect(() => {
+    if (count > Data.places.length-1) {
+      setCount(0);
+    } else if (count < 0) {
+      setCount(2);
+    }
+  }, [count]);
+
+  useEffect(() => {
+    console.log(count)
+    if (imageCount > (Data.places[count].image.length-1)) {
+      setImageCount(0);
+    }
+  },[count, imageCount])
+
+
   return (
     <div className="h-screen font-pixelify text-white tracking-widest flex flex-col justify-center items-center font-bold drop-shadow-[0_2px_2px_black]">
       <h1 className="text-4xl font-bold ">
-        <ReactTyped
+        {/* <ReactTyped
           strings={["Places I've been for the past months...  "]}
           typeSpeed={50}
-        />
+        /> */}
+        Places I've been for the past months...
       </h1>
       {/* Div for places/content */}
-      <div className="flex justify-center items-center gap-x-2 my-4 w-1/2 h-1/2">
-        <button>
+      <div className="flex justify-centera items-center gap-x-2 my-4 w-1/2 h-96 ">
+        <button onClick={prevButton}>
           <img src="images/left.png" className="w-10 h-10" />
         </button>
+
         <div className="border-8 p-4 w-full h-full rounded-lg border-yellow-800 bg-yellow-500">
-          <h1 className="drop-shadow-[0_2px_2px_black]">
-            <ReactTyped
-              strings={[
-                `   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.`,
-              ]}
-              typeSpeed={10}
-            />
-          </h1>
+          {Data.places
+            ? Data.places.map((place, index) => (
+                <div key={index}>
+                  {index === count ? (
+                    <div className="flex flex-col justify-center items-center">
+                      <h1 className="text-2xl drop-shadow-[0_0_2px_black]">
+                        {" "}
+                        {place.name}
+                      </h1>
+
+                      <div className="flex justify-center items-center my-4">
+                        <button onClick={prevImage}>
+                          {" "}
+                          <img
+                            src="images/leftImage.png"
+                            className="w-10 h-10"
+                          />
+                        </button>
+
+                        <img
+                          src={place.image[imageCount]}
+                          className="w-64 h-64"
+                        />
+                        <button onClick={nextImage}>
+                          {" "}
+                          <img
+                            src="images/rightImage.png"
+                            className="w-10 h-10"
+                          />{" "}
+                        </button>
+
+                        <h1 className="text-xl drop-shadow-[0_0_2px_black] m-7">
+                          {" "}
+                          {place.description}
+                        </h1>
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              ))
+            : null}
         </div>
-        <button>
+
+        <button onClick={nextButton}>
           {" "}
           <img src="images/right.png" className="w-10 h-10" />
         </button>
---
+++ frontend/src/data/data.jsx
@@ -0,0 +1,30 @@
+export const Data = {
+  places: [
+    {
+      name: "Mall of Asia Complex, Pasay City",
+      description: "Outh Taking of my Mechanical Engineer brother. ",
+      image: ["images/smx/1.png", "images/smx/2.png", "images/smx/3.png"],
+    },
+    {
+      name: "Concepcion, Tarlac",
+      description: "Annual family gathering at my Uncle's farm with a pool.",
+      image: [
+        "images/tarlac/1.png",
+        "images/tarlac/2.png",
+        "images/tarlac/3.png",
+      ],
+    },
+    {
+      name: "Fabrikas, Sta. Clara Rd, Guagua, Pampanga",
+      description: "Going out with the fam and eating together.",
+      image: [
+        "images/fabrikas/1.png",
+        "images/fabrikas/2.png",
+        "images/fabrikas/3.png",
+        "images/fabrikas/4.png",
+      ],
+    },
+  ],
+
+  
+};
