6c62bd0f67807895dcaa43860dcc8b3d5afbb481
Nash Maglaqui <nashmglq89@gmail.com>
2025-05-21 20:51:04 +0800

    Feat: bg music

--- frontend/src/screen/home.jsx
+++ frontend/src/screen/home.jsx
@@ -1,21 +1,67 @@
 import { Places, Welcome } from "../component/places";
 import { ReactTyped } from "react-typed";
 import { Projects } from "../component/projects";
-import { useState } from "react";
-import sound from "../music.mp3"
+import { useEffect, useState, useRef } from "react";
+import sound from "../music.mp3";
 export const Home = () => {
-  useState(() => {
-    new Audio(sound)
-  })
-  
+  const audioRef = useRef(null);
+  const [play, isPlay] = useState(false);
+
+  useEffect(() => {
+    // get current value of ref then change it
+    audioRef.current = new Audio(sound);
+
+    const playSound = () => {
+      audioRef.current
+        .play()
+        // if the .play() function is good then do this
+        .then(() => {
+          isPlay(true);
+        })
+        .catch((error) => {
+          console.error("Error: ", error);
+        });
+    };
+
+    playSound();
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
+      }
+    };
+  }, []);
+
+  const toggleButton = () => {
+    // this will pause/play the audio
+    if (play) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    // this will mute it
+    isPlay(!play);
+  };
+
   return (
     <div>
+      <button
+        onClick={toggleButton}
+        className="fixed top-4 right-4 z-50 rounded-full bg-neutral-800 w-10 h-0"
+      >
+        {play ? "on" : "off"}
+      </button>
+
       <div className="min-h-screen flex justify-center items-center font-pixelify tracking-widest text-white font-bold drop-shadow-[0_2px_2px_black] p-4">
         {/* Container */}
         <div className="flex w-5/11 md:w-5/6 lg:w-4/6 flex-wrap justify-center items-center">
           {/* Div of image */}
           <div className="flex w-full md:w-1/2 items-center justify-center ">
-            <img src="images/hello.png" className="h-auto drop-shadow-[0_2px_2px_black]" />
+            <img
+              src="images/hello.png"
+              className="h-auto drop-shadow-[0_2px_2px_black]"
+            />
           </div>
           {/* Div of image */}
 
@@ -38,7 +84,7 @@ export const Home = () => {
         </div>
       </div>
       <Places />
-      <Projects/>
+      <Projects />
     </div>
   );
 };
